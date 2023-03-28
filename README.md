16ad6a5eb5c4d2263d15308024be22753b048f57
Ansari Umer <umer37454@gmail.com>
2023-03-28 15:06:23 +0530

    react components

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Getting Started with Create React App
+# Quote it full stack web app
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
--- v1/script.js
+++ v1/script.js
@@ -15,12 +15,12 @@ btnShare.addEventListener("click", () => {
 
 //fetching data
 async function loadquotes() {
-  const res = await fetch("https://djtfohzgnwovwqeglscb.supabase.co/rest/v1/quotes", {
-    headers: {
-      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdGZvaHpnbndvdndxZWdsc2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4ODQ4ODQsImV4cCI6MTk5NDQ2MDg4NH0.iMzpoZe50FBX4UMG7tTx7wjltqZFWL_m1sgxGMfyW9k",
-      authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdGZvaHpnbndvdndxZWdsc2NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4ODQ4ODQsImV4cCI6MTk5NDQ2MDg4NH0.iMzpoZe50FBX4UMG7tTx7wjltqZFWL_m1sgxGMfyW9k"
-    }
-  });
+  // const res = await fetch("https://djtfohzgnwovwqeglscb.supabase.co/rest/v1/quotes", {
+  //   headers: {
+  //     apikey: 
+  //     authorization: 
+  //   }
+  // });
 
   const data = await res.json();
   console.log(data);
